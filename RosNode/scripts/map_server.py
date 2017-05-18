bab6937344b35a7e02f7c4cb5041d276bfdfcf62
Toni <funmitoni2@yahoo.co.uk>
2017-05-18 05:09:12 -0400

    Wrote up mapping code
    The EKF node needs the IMU fix (maybe retrying the accelerometer fix) the new IMU needs to be used :)

--- RosNode/scripts/map_server.py
+++ RosNode/scripts/map_server.py
@@ -49,16 +49,17 @@ class SensorUpdateThread(Thread):
         origin.orientation.y = 0
         origin.orientation.z = 0
         origin.orientation.w = 1
+        self._max_occupancy = 0.75
+        self._scan_tolerance = 0.025
         self._map.info.origin = origin
-        self._map.data = ([-1] * self._map.info.width) * self._map.info.height
-        self._car_radius = 0.12 / self._map.info.resolution
+        self._map.data = ([50] * self._map.info.width) * self._map.info.height
+        self._car_radius = 0.12
 
         #TODO the robot starts from the origin fix this
         self._old_position = np.matrix([[0],
-                                    [0],
-                                    [0]])
+                                        [0],
+                                        [0]])
 
-        self._ray_angle = atan((1.5 * self._map.info.resolution)/ 2.5)
         ultrasonic_sensors = str(rospy.get_param("~mapping_sensors")).split(",")
 
         for sensor in ultrasonic_sensors:
@@ -71,40 +72,59 @@ class SensorUpdateThread(Thread):
         self._update_map.release()
 
     def run(self):
-        self.robotSpace()
+        self.fillCells(0,0, (2*pi), 0, self._car_radius)
         while self._run:
+            self._update_map.acquire()
             self._map_publisher.publish(self._map)
+            self._update_map.release()
             time.sleep(1)
-#           if self._update_map.acquire():
-#               if not(self._run):
-#                   break
-#               pass#put map update here
 
     def close(self):
         self._run = False
 
-    def robotSpace(self):
-        self._update_map.acquire()
+    def fillCells(self, center_x, center_y, fov, yaw, radius):
         #ray tracing from 0 to 359 degrees
-        neg_bound = -int((pi / self._ray_angle) )
+        fov /= 2.0 #TODO to make life easier
+        ray_angle = (-0.0149) * radius + 0.039
+        neg_bound = -int(fov/ ray_angle)
         pos_bound = -neg_bound
         list_of_used_cells = []
-        max_rad = self._car_radius
 
-        #TODO convert the nested for loops below into some kind of arc filling function
+        #set the cell at the source of the ray to not occupied
+        x = int(center_x/self._map.info.resolution + self._map.info.width/2)
+        y = int(center_y/self._map.info.resolution + self._map.info.height/2)
+
+        def odds(x): return ((x / (1.0001-x), x / (1-x))[x == 1.0])
+
+        self._update_map.acquire()
+        #TODO switch to bresemhams algorithm?
         for theta in range(neg_bound, pos_bound,1):
-            for r in range(int(max_rad/self._map.info.resolution), 1, -1):
+            for r in range(int((radius*(1 + self._scan_tolerance))/self._map.info.resolution), 0, -1):
                 #center point of the boundary of the scan
-                r = r * self._map.info.resolution
-                x_p = int(self._map.info.width/2 + r*cos(theta*self._ray_angle))
-                y_p = int(self._map.info.height/2 + r*sin(theta*self._ray_angle))
-                
-                if not((x_p,y_p) in list_of_used_cells):
-                    self._map.data[y_p * self._map.info.width + x_p] = 0
-                    list_of_used_cells.append((x_p,y_p))
+                x_p = int(x + r*cos(yaw + theta*ray_angle))
+                y_p = int(y + r*sin(yaw + theta*ray_angle))
 
+                if not((x_p,y_p) in list_of_used_cells):
+                    if (fov == pi):
+                        #TODO add a tolerance region around this
+                        self._map.data[y_p * self._map.info.width + x_p] = 0
+                    else:
+                        #TODO handle the 2.5
+                        last_p_occ = self._map.data[y_p * self._map.info.width + x_p] / 100.0
+                        m_occ = self.logOddsUpdate(r * self._map.info.resolution, theta*ray_angle, radius, 2.5, fov)
+                        occ = odds(m_occ) * odds(last_p_occ) * 1
+                        #print "probs: " + str(m_occ) + " " + str(last_p_occ) + " " + str((occ)/(1+(occ)))
+                        self._map.data[y_p * self._map.info.width + x_p] = max(1,min(99,((occ)/(1+(occ))) * 100))
+                    list_of_used_cells.append((x_p,y_p))
         self._update_map.release()
-        pass
+
+    def logOddsUpdate(self, r, theta, scan, max_scan, fov):
+        theta = abs(theta)
+        if (r > (scan * (1 - self._scan_tolerance))):
+            p_occ = 0.75#(((max_scan - r)/max_scan) + ((fov-theta)/fov)) * self._max_occupancy / 2.0
+        else:
+            p_occ = 0.25#(1 - (((max_scan - r)/max_scan) + ((fov-theta)/fov)) / 2.0)
+        return p_occ
 
     def updateRobotPosition(self, data):
         self._position_mutex.acquire()
@@ -112,9 +132,9 @@ class SensorUpdateThread(Thread):
         self._position = np.matrix([[data.pose.pose.position.x],
                                     [data.pose.pose.position.y],
                                     [yaw]])
-        
+
         if sqrt(((self._position[0] - self._old_position[0])**2) + ((self._position[1] - self._old_position[1])**2)) >=  self._map.info.resolution:
-            self.robotSpace()
+            self.fillCells(0,0, (2*pi), 0, self._car_radius)
 
         #the robot is represented by a circle
         self._old_position = self._position
@@ -122,7 +142,6 @@ class SensorUpdateThread(Thread):
 
     def ultrasonicCallback(self, data):
         now = rospy.Time(0)
-        #TODO replace the base_link below with the map to get the global transform
         self._listener.waitForTransform(self._map.header.frame_id, data.header.frame_id, now ,rospy.Duration(0.25))
         (trans,rot) = self._listener.lookupTransform(self._map.header.frame_id, data.header.frame_id, now)
 
@@ -131,33 +150,7 @@ class SensorUpdateThread(Thread):
         self._position_mutex.release()
         (roll,pitch,yaw) = euler_from_quaternion([rot[0],rot[1], rot[2], rot[3]])
 
-        #set the cell at the source of the ray to not occupied
-        x = int(trans[0]/self._map.info.resolution + self._map.info.width/2)
-        y = int(trans[1]/self._map.info.resolution + self._map.info.height/2)
-
-        self._map.data[y * self._map.info.width + x] = -100
-
-        #set the boundary of the scan on the map
-        r = 1/ self._map.info.resolution #data.range/self._map.info.resolution
-        
-        #angle of iteration
-        neg_bound = -int((data.field_of_view / self._ray_angle) / 2)
-        pos_bound = -neg_bound
-
-        list_of_used_cells = []
-
-        self._update_map.acquire()
-        for theta in range(neg_bound, pos_bound,1):
-            #center point of the boundary of the scan
-            x_p = int(x + r*cos(yaw+(theta*self._ray_angle)))
-            y_p = int(y + r*sin(yaw+(theta*self._ray_angle)))
-
-            if not((x_p,y_p) in list_of_used_cells):
-                self._map.data[y_p * self._map.info.width + x_p] = -100
-                list_of_used_cells = []
-                list_of_used_cells.append((x_p,y_p))
-
-        self._update_map.release()
+        self.fillCells(trans[0], trans[1], data.field_of_view, yaw, 2.5)#data.range)
 
 if __name__ == '__main__':
     main()
